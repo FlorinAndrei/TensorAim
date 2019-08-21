0ec58e404967100bb445e426da9e66f6001e104d
Florin Andrei <florin.andrei@sage.com>
2019-08-20 18:36:47 -0700

    generate whole set, pickle it

--- gen_test_train_data.py
+++ gen_test_train_data.py
@@ -5,6 +5,8 @@ import numpy as np
 import cairo
 import random
 from PIL import Image
+import pickle
+import gc
 from pprint import pprint
 
 
@@ -50,39 +52,68 @@ def fuzzy_circle(ctext, mrad, thick, steps, w, h):
 width, height = 120, 90
 tgrad, tgthick, tgstep = 10, 1, 3
 snrad = 4
+setsize = 10000
 
 raster = np.zeros(shape=(height, width), dtype=np.uint32)
 workspace = cairo.ImageSurface.create_for_data(raster, cairo.FORMAT_RGB24, width, height)
 cc = cairo.Context(workspace)
 
-# fill with white
-cc.set_source_rgb(1, 1, 1)
-cc.paint()
+print('Generating image set with', setsize, 'images:')
 
-# paint the target
-x, y = fuzzy_circle(cc, tgrad, tgthick, tgstep, width, height)
+train_set = []
 
-# make it snow
-snow(cc, snrad, int(width * height / 1000), width, height)
+for step in range(setsize):
+  print(step, end='\r', flush=True)
+  # fill with white
+  cc.set_source_rgb(1, 1, 1)
+  cc.paint()
+  
+  if step % 4 == 0:
+    # no target
+    x, y= 0, 0
+    is_target = False
+  else:
+    # paint the target
+    x, y = fuzzy_circle(cc, tgrad, tgthick, tgstep, width, height)
+    is_target = True
+  
+  # make it snow
+  snow(cc, snrad, int(width * height / 1000), width, height)
+  
+  # convert the workspace to a monochrome 8 bit image
+  
+  # make an output raster with all pixels decomposed as RGB triplets
+  buf = workspace.get_data()
+  rasout = np.ndarray(shape=(height, width, 4), dtype=np.uint8, buffer=buf)
+  # slice off the unused alpha channel
+  rasout = rasout[:, :, :3]
+  # convert to mono by flattening the 3D matrix
+  rasout = np.mean(rasout, axis=2)
+  # maximize contrast by expanding the range to 0-255
+  rmin = np.min(rasout)
+  rmax = np.max(rasout)
+  output = np.empty(np.shape(rasout), dtype=np.uint8)
+  output = np.interp(rasout, [rmin, rmax], [0, 255]).astype(np.uint8)
+  
+  # save image
+  #im = Image.fromarray(output, 'L')
+  #im.save('test.png')
+  
+  # Make one training object - a list with:
+  # - whether the target exists or not
+  # - the coordinates of the center of the target
+  # - the whole image
+  train_obj = []
+  train_obj.append(is_target)
+  train_obj.append([x, y])
+  train_obj.append(output)
+  # append the object to the training set
+  # bug with garbage collection makes append() very slow; turn it off
+  gc.disable()
+  train_set.append(train_obj)
+  gc.enable()
 
+# pickle the set
+pickle.dump(train_set, open('train_set.p', 'wb'))
 
-# convert the workspace to a monochrome 8 bit image
-
-# make an output raster with all pixels decomposed as RGB triplets
-buf = workspace.get_data()
-rasout = np.ndarray(shape=(height, width, 4), dtype=np.uint8, buffer=buf)
-# slice off the unused alpha channel
-rasout = rasout[:, :, :3]
-# convert to mono by flattening the 3D matrix
-rasout = np.mean(rasout, axis=2)
-# maximize contrast by expanding the range to 0-255
-rmin = np.min(rasout)
-rmax = np.max(rasout)
-output = np.empty(np.shape(rasout), dtype=np.uint8)
-output = np.interp(rasout, [rmin, rmax], [0, 255]).astype(np.uint8)
-
-# save image
-im = Image.fromarray(output, 'L')
-im.save('test.png')
-
-# TODO: pickle
+print('All done.')

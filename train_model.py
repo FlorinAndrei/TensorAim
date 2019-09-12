85ed0f6bed655067d579d26d69bbd106c794fcd5
Florin Andrei <florin@andrei.myip.org>
2019-09-12 14:46:02 -0700

    remove MNIST trial

--- train_model.py
+++ train_model.py
@@ -69,14 +69,6 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                  verbose=1,
                                                  save_freq='epoch')
 
-"""
-# load train/test data
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-"""
-
 print('load data')
 syndata = pickle.load(open('synthetic_data.p', 'rb'))
 syntot = len(syndata)

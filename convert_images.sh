e635a7465b3f42b753516f75fca5427d120d874c
Florin Andrei <florin@andrei.myip.org>
2019-09-14 17:45:17 -0700

    first commit

---
+++ convert_images.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+rm -rf images/small
+mkdir images/small
+
+# better algorithm than simple "convert -resize"
+# keeps ringing artifacts smaller
+find images/original/* | cut -d / -f 3 | parallel \
+  convert images/original/{} -colorspace RGB +sigmoidal-contrast 6.5,50% \
+    -filter Lanczos -distort resize 120x90 \
+	-sigmoidal-contrast 6.5,50% -colorspace Gray images/small/{}.png

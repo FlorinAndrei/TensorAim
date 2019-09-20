00f1ef626dc3f3e88760c26c93514643de838e9f
Florin Andrei <florin@andrei.myip.org>
2019-09-19 18:09:55 -0700

    first commit

---
+++ camera_live_test.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import time
+
+# use DSHOW to get rid of the letterboxed format
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# use the smallest available resolution
+ret = cap.set(3,640)
+ret = cap.set(4,480)
+
+model = keras.models.load_model('saved_model.h5')
+
+while(True):
+  t1 = int(round(time.time() * 1000))
+  # capture frame
+  ret, frame = cap.read()
+  # grayscale
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  # scale down
+  fframe = cv2.resize(gray, (120, 90), interpolation=cv2.INTER_AREA)
+  # display
+  cv2.imshow('camera', fframe)
+  show2net = fframe.reshape(1, 90, 120, 1) / 255.0
+  p = model.predict(show2net)
+  t2 = int(round(time.time() * 1000))
+  if np.argmax(p) == 0:
+    pred = 'square'
+  if np.argmax(p) == 1:
+    pred = 'circle'
+  print(pred, '\t', int(1000/(t2-t1)), 'fps')
+  # quit
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+cap.release()

29d0d964529b4b15b87fc0e0fb72f03e58ddd3f5
Florin Andrei <florin@andrei.myip.org>
2019-09-22 16:28:06 -0700

    shuffle the dataset

--- train_real_world_model.py
+++ train_real_world_model.py
@@ -53,6 +53,11 @@ h5.close()
 lentotal = images.shape[0]
 lentrain = int(lentotal * 80 / 100)
 
+randindex = np.arange(lentotal)
+np.random.shuffle(randindex)
+images = images[randindex, :, :, :]
+labels = labels[randindex]
+
 images_train = np.empty((lentrain, 90, 120, 1), dtype=np.float32)
 labels_train = np.empty((lentrain), dtype=np.uint8)
 images_tests = np.empty((lentotal - lentrain, 90, 120, 1), dtype=np.float32)

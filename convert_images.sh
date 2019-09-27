3c76419e36bb0cae7c32443ac3199b2cf484fff4
Florin Andrei <florin@andrei.myip.org>
2019-09-26 19:39:18 -0700

    parametrize the script

--- convert_images.sh
+++ convert_images.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-rm -rf images/small
-mkdir images/small
+imshape="120x90"
+outpath="images/small"
+
+rm -rf ${outpath}
+mkdir ${outpath}
 
 # better algorithm than simple "convert -resize"
 # keeps ringing artifacts smaller
 # https://imagemagick.org/discourse-server/viewtopic.php?f=1&t=36709
 find images/original/* | cut -d / -f 3 | parallel \
   convert images/original/{} -colorspace Gray \
-    -filter Triangle -resize 120x90 \
-    images/small/{}.png
+    -filter Triangle -resize ${imshape} \
+    ${outpath}/{}.png

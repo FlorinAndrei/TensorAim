918e52ad46742decb78ce60793c74c6784ffd228
Florin Andrei <florin@andrei.myip.org>
2019-10-06 02:02:01 -0700

    cleanup import section

--- train.py
+++ train.py
@@ -1,13 +1,6 @@
-# create a YOLOv3 Keras model and save it to file
-# based on https://github.com/experiencor/keras-yolo3
 import struct
 import numpy as np
-from keras.layers import Conv2D
-from keras.layers import Input
-from keras.layers import BatchNormalization
-from keras.layers import LeakyReLU
-from keras.layers import ZeroPadding2D
-from keras.layers import UpSampling2D
+from keras.layers import Conv2D, Input, BatchNormalization, LeakyReLU, ZeroPadding2D, UpSampling2D
 from keras.layers.merge import add, concatenate
 from keras.models import Model
 import os

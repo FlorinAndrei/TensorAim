d3172cd85a9f9e7cda0667d2ad638dfceaf64999
Florin Andrei <florin@andrei.myip.org>
2019-10-06 00:10:47 -0700

    clarify image resolution choice

--- sentry.py
+++ sentry.py
@@ -234,6 +234,7 @@ anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33
 # define the probability threshold for detected objects
 class_threshold = 0.6
 
+# no need to capture in hi-def if the model can't do that
 cam_w, cam_h = 640, 480
 if args.defdriver:
   # use default system driver
